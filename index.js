b2fe79eec0fe9b3cb5eb4b39d641fe706f620e9d
JackShepee <salamakhaviktor@gmail.com>
2023-06-12 22:15:50 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const sum = (function () {
+  let total = 0;
+
+  return function (num) {
+    total += num;
+    return total;
+  };
+})();
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
